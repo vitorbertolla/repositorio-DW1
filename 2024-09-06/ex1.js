e76843cf5ca5539e18e542c64d1e80365649bafe
Vitor Hugo Almeida Bertolla <163414035+vitorbertolla@users.noreply.github.com>
2024-09-09 09:15:34 -0300

    .

---
+++ 2024-09-06/ex1.js
@@ -0,0 +1,18 @@
+console.log("1")
+console.log("2")
+let condicao1 = True
+let condicao2 = False
+let condicao3 = True
+if(condicao1==True){
+
+    if(condicao3==True){
+        console.log("A")
+    }
+    else{
+        console.log("B")
+    }
+}else if(condicao2==True){
+    console.log("C")
+}else{
+    
+}
